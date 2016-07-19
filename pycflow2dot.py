d534cd9d1d1b9f350566d7d869cb5d6ce94207ee
fuga <yabu76@gmail.com>
2016-07-20 07:49:09 +0900

    change output file name counter max 99 to 9999.

--- pycflow2dot.py
+++ pycflow2dot.py
@@ -373,7 +373,7 @@ def write_graphs2dot(graphs, c_fnames, img_fname, for_latex, multi_page, layout)
         if img_fname == '@':
             cur_img_fname = 'cflow_' + c_fname.replace('.', '_')
         else:
-            cur_img_fname = img_fname + ('_%02u' % counter)
+            cur_img_fname = img_fname + ('_%04u' % counter)
             counter += 1
             
         dot_paths += [write_graph2dot(graph, other_graphs, c_fname, cur_img_fname,
